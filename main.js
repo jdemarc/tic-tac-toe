1f501c3bebdd36ab78b7c13dd00eb31e3fcde3f3
joseph <josephjohndemarco@gmail.com>
2020-08-01 18:53:38 -0400

    Correct winner assignment. Add comments.

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const WIN_TABLE = [
     [1, 4, 7],
     [2, 5, 8],
 
-    [0, 4, 8], // Diagnol win.
+    [0, 4, 8], // Diagonal win.
     [2, 4, 6]
 ];
 
@@ -25,8 +25,10 @@ let turn;
 let winner;
 
 /*----- cached element references -----*/
+// Select all direct children of the game-grid id.
 const squareEls = document.querySelectorAll('#game-grid > div');
 
+//Select the h2 id='score msg' element that displays messages to user.
 const msgEl = document.getElementById('score-msg');
 
 /*----- event listeners -----*/
@@ -39,34 +41,42 @@ document.getElementById('reset')
 /*----- functions -----*/
 init();
 
+// Click handling
 function handleSquareClick(event) {
     let squareId = event.target.id;
+
     //Extract index of square from div id using regex.
     squareIdx = squareId.replace(/\D/g, '');
 
-    console.log('Square index', squareIdx);
-    
+    /**
+     * If the user attempts to click on a value that is not null, that means the square was already
+     * clicked and assigned a value.  The function is exited and awaits the next click.
+     * If the winner variable is assigned a number or 'T', the game is over (win or tie).
+     */ 
+
     if (
         board[squareIdx] !== null ||
-        winner !== null
+        winner
     ) return;
 
+    // Assign the player value to the square that was clicked.
     board[squareIdx] = turn;
-    turn *= -1;
     
-    for (let i = 0; i < WIN_TABLE.length; i++) {
-        if (Math.abs(board[WIN_TABLE[i][0]]) + 
-            (board[WIN_TABLE[i][1]]) +
-            (board[WIN_TABLE[i][2]]) === 3) {
-                winner = board[WIN_TABLE[i][0]];
-        } else if (board.includes(!null)) {
-            winner = 'T';
-        }
-    }
+    // Change current player.
+    turn *= -1;
+
+    // Test board for a winner, a tie, or if game is in play.
+    winner = testForWin();
 
     renderBoard();
 }
 
+function handleResetClick() {
+    init();
+    renderBoard();
+}
+
+// Initialize board
 function init() {
     board = Array(9).fill(null);
     turn = 1;
@@ -75,11 +85,15 @@ function init() {
     renderBoard();
 }
 
+// Render functions
 function renderBoard() {
 
-    // Iterate over the board and give each square a gray background.
-    // Since the board should be initialized to null, color['null'] should be gray.
-
+    /**
+     * Iterate over the board and give each square an initial background.
+     * Since the board is filled with null, 
+     * the color assigned to the object null property will be displayed.
+     */  
+    
     board.forEach(function(square, idx) {
         squareEls[idx].style.background = colors[square];
     });
@@ -89,16 +103,42 @@ function renderBoard() {
 
 function renderMessage() {
 
-    if (winner) {
-        msgEl.textContent = `${colors[winner].toUpperCase()} has won.`; 
-    } else if (winner === 'T') {
-        msgEl.textContent = 'Tie';
-    } else {
+    /**
+     * If the winner === null, the game continues and prompts the next player to play.
+     * If winner contains a number, it means a winner was found.
+     * Otherwise, the winner === 'T', and the game has tied.
+     */
+    if (winner === null) {
         msgEl.textContent = `${colors[turn].toUpperCase()}'s turn.`;
+    } else if (typeof winner === 'number') {
+        msgEl.textContent = `${colors[winner].toUpperCase()} has won.`; 
+    } else {
+        msgEl.textContent = 'Tie game!'
     }
 }
 
-function handleResetClick() {
-    init();
-    renderBoard();
+function testForWin() {
+    /**
+     * Iterate through the win table.
+     * Each board value is set to 1 or -1.
+     * If the absolute value of the board values that match with any of the win table arrays equal 3,
+     * a winner has been found. Assign the winner the value of the first index of the matched array.
+     */
+
+    for (let i = 0; i < WIN_TABLE.length; i++) {
+        if (Math.abs(board[WIN_TABLE[i][0]] + board[WIN_TABLE[i][1]] + board[WIN_TABLE[i][2]]) === 3) {
+                return board[WIN_TABLE[i][0]];
+            }   
+        }
+
+    /**
+     * If the board contains null values, the winner is set to null so the game can continue to play.
+     * Otherwise, if the board is filled with values but no winner was found, the game has tied.
+     */
+
+    if (board.includes(null)) {
+        return null;
+    } else {
+        return 'T';
+    }
 }
